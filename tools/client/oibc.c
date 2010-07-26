87db2a4a52b1dfd4b2503bf8ec5e0424f74537d0
nickpack <nick@nickpack.com>
2010-07-26 10:19:49 +0100

    Backup NOR in oibc when install command is issued (alexandred)

--- tools/client/oibc.c
+++ tools/client/oibc.c
@@ -182,18 +182,18 @@ void* doInput(void* threadid) {
             if (getFile(commandBuffer)==1)
                 continue;
             
-        } else if (strcmp(commandBuffer,"backup_nor") == 0) {
+        } else if (strcmp(commandBuffer,"install") == 0) {
+            printf("Backing up your NOR to current directory as norbackup.dump\n");
             sprintf(toSendBuffer, "nor_read 0x09000000 0x0 1048576");
-            
+            sprintf(toSendBuffer,"~norbackup.dump:1048576"); 
+            getFile(toSendBuffer);
+	    	sendBuffer("install", len + 1); 
             InterestWrite = 1;
             commandBuffer[len] = '\n';
             pthread_mutex_lock(&lock);
             sendBuffer(toSendBuffer, strlen(toSendBuffer));
             pthread_mutex_unlock(&lock);
             InterestWrite = 0;
-            
-            sprintf(toSendBuffer,"~norbackup.dump:1048576"); 
-            getFile(toSendBuffer);
 		} else {
 			InterestWrite = 1;
 			commandBuffer[len] = '\n';
@@ -307,7 +307,6 @@ done:
 
 	printf("Client connected:\n");
     printf("!<filename>[@<address>] to send a file, ~<filename>[@<address>]:<len> to receive a file\n");
-    printf("type backup_nor to make a backup of your NOR before installing\n");
 	printf("---------------------------------------------------------------------------------------------------------\n");
 
 	pthread_create(&outputThread, NULL, doOutput, NULL);
@@ -318,4 +317,3 @@ done:
 	usb_release_interface(device, i);
 	usb_close(device);
 }
-
