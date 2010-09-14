c4341b7dd03c4566427ad27d46e9a7c6ab3546af
nickpack <nick@nickpack.com>
2010-09-14 08:38:33 +0100

    Fixed multi-touch bug in z2 (Credit to Noltari)

--- openiboot/multitouch-z2.c
+++ openiboot/multitouch-z2.c
@@ -1052,7 +1052,7 @@ int multitouch_ispoint_inside_region(uint16_t x, uint16_t y, int w, int h)
     fingerX = (fingerData->x * framebuffer_width()) / SensorWidth - 2;
     fingerY = ((SensorHeight - fingerData->y) * framebuffer_height()) / SensorHeight - 2;
     
-    if (fingerX>=x && fingerX<= (x + w)  && fingerY>=y && fingerY<= (y + w) ) {
+    if (fingerX>=x && fingerX<= (x + w)  && fingerY>=y && fingerY<= (y + h) ) {
         return TRUE;
     }
     
