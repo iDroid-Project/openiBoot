333e31ddc041b950138327ffc89e89638541bbcf
Patrick Wildt <webmaster@patrick-wildt.de>
2010-10-29 18:12:20 +0200

    Updated interrupt_setup to match iBoot and ifdefd VIC2 and VIC3.

--- openiboot/interrupt.c
+++ openiboot/interrupt.c
@@ -6,6 +6,7 @@
 #include "openiboot-asmhelpers.h"
 
 int interrupt_setup() {
+#ifndef CONFIG_IPHONE_4G
 	if((0xfff & (GET_REG(VIC0 + VICPERIPHID0) | (GET_REG(VIC0 + VICPERIPHID1) << 8) | (GET_REG(VIC0 + VICPERIPHID2) << 16) | (GET_REG(VIC0 + VICPERIPHID3) << 24))) != 0x192) {
 		/* peripheral ID for vic0 doesn't match PL 192, which is the VIC we are expecting */
 		return -1;
@@ -16,43 +17,40 @@ int interrupt_setup() {
 		return -1;
 	}
 
-#ifdef CONFIG_IPHONE_4G
-	if((0xfff & (GET_REG(VIC2 + VICPERIPHID0) | (GET_REG(VIC2 + VICPERIPHID1) << 8) | (GET_REG(VIC2 + VICPERIPHID2) << 16) | (GET_REG(VIC2 + VICPERIPHID3) << 24))) != 0x192) {
-		/* peripheral ID for vic2 doesn't match PL 192, which is the VIC we are expecting */
-		return -1;
-	}
-
-	if((0xfff & (GET_REG(VIC3 + VICPERIPHID0) | (GET_REG(VIC3 + VICPERIPHID1) << 8) | (GET_REG(VIC3 + VICPERIPHID2) << 16) | (GET_REG(VIC3 + VICPERIPHID3) << 24))) != 0x192) {
-		/* peripheral ID for vic3 doesn't match PL 192, which is the VIC we are expecting */
-		return -1;
-	}
-#endif
-
 	SET_REG(EDGEIC + EDGEICCONFIG0, EDGEIC_CONFIG0RESET);
 	SET_REG(EDGEIC + EDGEICCONFIG1, EDGEIC_CONFIG1RESET);
+#endif
 
 	SET_REG(VIC0 + VICINTENCLEAR, 0xFFFFFFFF); // disable all interrupts
 	SET_REG(VIC1 + VICINTENCLEAR, 0xFFFFFFFF);
+#ifdef CONFIG_IPHONE_4G
 	SET_REG(VIC2 + VICINTENCLEAR, 0xFFFFFFFF);
 	SET_REG(VIC3 + VICINTENCLEAR, 0xFFFFFFFF);
+#endif
 
 	SET_REG(VIC0 + VICINTSELECT, 0); // 0 means to use IRQs, 1 means to use FIQs, so use all IRQs
 	SET_REG(VIC1 + VICINTSELECT, 0);
+#ifdef CONFIG_IPHONE_4G
 	SET_REG(VIC2 + VICINTSELECT, 0);
 	SET_REG(VIC3 + VICINTSELECT, 0);
+#endif
 
 	SET_REG(VIC0 + VICSWPRIORITYMASK, 0xffff); // unmask all 16 interrupt levels
 	SET_REG(VIC1 + VICSWPRIORITYMASK, 0xffff);
+#ifdef CONFIG_IPHONE_4G
 	SET_REG(VIC2 + VICSWPRIORITYMASK, 0xffff);
 	SET_REG(VIC3 + VICSWPRIORITYMASK, 0xffff);
+#endif
 
 	// Set interrupt vector addresses to the interrupt number. This will signal the interrupt handler to consult the handler tables
 	int i;
 	for(i = 0; i < VIC_InterruptSeparator; i++) {
 		SET_REG(VIC0 + VICVECTADDRS + (i * 4), i);
 		SET_REG(VIC1 + VICVECTADDRS + (i * 4), VIC_InterruptSeparator + i);
+#ifdef CONFIG_IPHONE_4G
 		SET_REG(VIC2 + VICVECTADDRS + (i * 4), VIC_InterruptSeparator*2 + i);
 		SET_REG(VIC3 + VICVECTADDRS + (i * 4), VIC_InterruptSeparator*3 + i);
+#endif
 	}
 
 	memset(InterruptHandlerTable, 0, sizeof(InterruptHandlerTable));
