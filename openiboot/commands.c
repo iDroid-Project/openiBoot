b12fb89886fdb950c713a5b9ddb7635baaba551a
Neonkoala <neonkoala@neonkoala-Compaq-Mini-110c-1000.(none)>
2010-11-06 11:04:22 +0000

    Added boot_ios command.

--- openiboot/commands.c
+++ openiboot/commands.c
@@ -350,7 +350,7 @@ void cmd_rootfs(int argc, char** argv) {
 	bufferPrintf("set rootfs to %s on partition %d\r\n", fileName, partition);
 }
 
-void cmd_boot(int argc, char** argv) {
+void cmd_boot_linux(int argc, char** argv) {
 	char* arguments = "";
 
 	if(argc >= 2) {
@@ -360,6 +360,15 @@ void cmd_boot(int argc, char** argv) {
 	bufferPrintf("Booting kernel with arguments (%s)...\r\n", arguments);
 
 	boot_linux(arguments);
+}
+
+void cmd_boot_ios(int argc, char** argv) {
+	Image* image = images_get(fourcc("ibox"));
+	if(image == NULL)
+		image = images_get(fourcc("ibot"));
+	void* imageData;
+	images_read(image, &imageData);
+	chainload((uint32_t)imageData);
 }
 
 void cmd_go(int argc, char** argv) {
@@ -1296,7 +1305,8 @@ OPIBCommand CommandList[] =
 		{"kernel", "load a Linux kernel", cmd_kernel},
 		{"ramdisk", "load a Linux ramdisk", cmd_ramdisk},
 		{"rootfs", "specify a file as the Linux rootfs", cmd_rootfs},
-		{"boot", "boot a Linux kernel", cmd_boot},
+		{"boot_linux", "boot a Linux kernel", cmd_boot_linux},
+		{"boot_ios", "boot a iOS via iBoot", cmd_boot_ios},
 		{"go", "jump to a specified address (interrupts disabled)", cmd_go},
 		{"jump", "jump to a specified address (interrupts enabled)", cmd_jump},
 		{"version", "display the version string", cmd_version},
