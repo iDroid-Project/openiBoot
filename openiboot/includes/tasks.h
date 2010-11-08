a0e19a696cc3379174411a7c2bf72b7fc9c14d5a
Ricky Taylor <rickytaylor26@gmail.com>
2010-11-08 04:38:20 +0000

    Known-position task switching initial implementation.

--- openiboot/includes/tasks.h
+++ openiboot/includes/tasks.h
@@ -3,6 +3,16 @@
 
 #include "openiboot.h"
 
+#define TASK_STACK_SIZE (1*1024*1024) // 1MB
+
 int tasks_setup();
+void tasks_run();
+
+void task_init(TaskDescriptor *_td, char *_name);
+void task_destroy(TaskDescriptor *_td);
+int task_start(TaskDescriptor *_td, void *_fn, void *_arg);
+void task_stop();
+
+int task_yield();
 
 #endif
