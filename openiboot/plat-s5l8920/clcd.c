3f9f125ace494b39b523f9b74bcbaca17e874aa9
Oranav <oranav@gmail.com>
2011-03-30 01:46:31 +0200

    clcd: Mehfu.

--- openiboot/plat-s5l8920/clcd.c
+++ openiboot/plat-s5l8920/clcd.c
@@ -207,7 +207,7 @@ int displaypipe_init() {
 	uint32_t curBuf;
 	buffer1[0] = 1;
 	for (curBuf = 0; curBuf != 256; curBuf++) {
-		if (signed_calculate_remainder(curBuf+1, (256 >> (10 - (uint8_t)(LCDTable->bitsPerPixel / 3)))) == 1)
+		if (((curBuf+1) % (256 >> (10 - (uint8_t)(LCDTable->bitsPerPixel / 3)))) == 1)
 			buffer1[curBuf] = buffer1[curBuf] - 1;
 		buffer1[curBuf+1] = buffer1[curBuf] + 4;
 		buffer2[curBuf] = buffer1[curBuf];
