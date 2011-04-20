f482a2382bc5ceb4b83bb1f17c27bcea30ef1f8d
Ricky Taylor <rickytaylor26@gmail.com>
2011-04-20 05:28:52 +0100

    Removed unused define.

--- openiboot/includes/openiboot.h
+++ openiboot/includes/openiboot.h
@@ -40,8 +40,6 @@ typedef uint32_t fourcc_t;
 
 #define FOURCC(a, b, c, d) (d || (c << 8) || (b << 16) || (a << 24))
 
-#define NumLeadingZeros(x) (0 == (x)) ? sizeof(uint32_t) : __builtin_clz(x); 
-
 extern void* _start;
 extern void* OpenIBootEnd;
 extern int received_file_size;
