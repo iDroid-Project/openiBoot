a6bfd88fe4818aeff8d0850f65b9d6f4e038cf27
Ricky Taylor <rickytaylor26@gmail.com>
2011-09-08 01:50:37 +0800

    Updated error.h to use __ for internal variables.

--- openiboot/includes/error.h
+++ openiboot/includes/error.h
@@ -33,7 +33,7 @@
 
 #define FAIL_ON(x, y)		do { if(x) { return y; } } while(0)
 #define SUCCEED_ON(x)		FAIL_ON((x), SUCCESS)
-#define CHAIN_FAIL(x)		do { error_t val = (x); if(FAILED(val)) { return val; } } while(0)
+#define CHAIN_FAIL(x)		do { error_t __val = (x); if(FAILED(__val)) { return __val; } } while(0)
 
 enum
 {
