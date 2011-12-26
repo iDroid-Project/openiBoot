2817030d311912dbb922f8df256406d9c22e0a3c
boxingcow <boxingcow@rocketmail.com>
2011-12-26 22:19:27 +0000

    menu.h is deprecated

--- openiboot/includes/menu.h
+++ openiboot/includes/menu.h
@@ -21,6 +21,12 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+
+/**
+ *  This is deprecated, replaced with menu module.
+ *
+ */
+
 #ifndef MENU_H
 #define MENU_H
 
