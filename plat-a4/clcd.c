dbe8d580e86f52f9fb240fd3498fc9c573e9c098
Patrick Wildt <webmaster@patrick-wildt.de>
2012-02-16 23:32:33 +0100

    clcd: memalign for framebuffer area, should've done that some time ago

--- plat-a4/clcd.c
+++ plat-a4/clcd.c
@@ -242,10 +242,6 @@ int displaypipe_init() {
 
 	clock_gate_switch(0xB, ON); // HPERF2
 
-	memset((void*)CLCD_FRAMEBUFFER, 0, 0x800000);
-
-	bufferPrintf("displaypipe_init: framebuffer address: 0x%08x\n", CLCD_FRAMEBUFFER);
-
 	if (!LCDTable)
 		LCDTable = &LCDInfoTable[DISPLAYID];
 
@@ -722,7 +718,7 @@ static Window* createWindow(int zero0, int zero2, int width, int height, ColorSp
 	newWindow->height = height;
 	newWindow->lineBytes = width * (bitsPerPixel / 8);
 
-	createFramebuffer(&newWindow->framebuffer, CLCD_FRAMEBUFFER, width, height, width, colorSpace);
+	createFramebuffer(&newWindow->framebuffer, (uint32_t)memalign(16, newWindow->lineBytes*height), width, height, width, colorSpace);
 
 	SET_REG(DISPLAY_PIPE + 0x4040, (reg_bit << 8) | 1);
 	SET_REG(DISPLAY_PIPE + 0x4044, (uint32_t)newWindow->framebuffer.buffer);
